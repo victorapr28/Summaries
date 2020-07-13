6a82c94e6dd636a6d00a1d886b4ac283035cb062
Victor <victorapr28@gmail.com>
2020-07-13 05:41:30 -0700

    Reading data from Angular

--- ClientApp/src/app/services/book.service.ts
+++ ClientApp/src/app/services/book.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  _baseURL = 'api/Books';
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks() {
+    return this.http.get<Book[]>(this._baseURL + '/GetBooks');
+  }
+
+  addBook(book: Book) {
+    return this.http.post(this._baseURL + '/AddBook/', book);
+  }
+
+  getBookById(id: number) {
+    return this.http.get<Book>(this._baseURL + '/SingleBook/' + id);
+  }
+
+  updateBook(book: Book) {
+    return this.http.put(this._baseURL + '/UpdateBook/' + book.id, book);
+  }
+
+  deleteBook(id: number) {
+    return this.http.delete(this._baseURL + '/DeleteBook/' + id);
+  }
 }
